1ee6733eb86a6016382cc58caf9421cb19344de2
Jasleen-Chahal <chahjasl@sheridancollege.ca>
2019-05-22 09:37:33 -0400

    initail commit

---
+++ src/studentweek3/Student.java
@@ -0,0 +1,30 @@
+package studentweek3;
+/**
+ * @author Jasleen kaur Chahal ()
+ */
+public class Student
+{
+   private int ID;
+   private String firstName;
+
+   public int getID ()
+   {
+      return ID;
+   }
+
+   public void setID (int ID)
+   {
+      this.ID = ID;
+   }
+
+   public String getFirstName ()
+   {
+      return firstName;
+   }
+
+   public void setFirstName (String firstName)
+   {
+      this.firstName = firstName;
+   }
+
+}
---
+++ src/studentweek3/StudentWeek3.java
@@ -0,0 +1,18 @@
+package studentweek3;
+
+/**
+ *
+ * @author Jasleen Kaur Chahal ()
+ */
+public class StudentWeek3
+{
+   /**
+    * @param args the command line arguments
+    */
+   public static void main (String[] args)
+   {
+      Student s1 = new Student();
+      s1.setFirstName("John Smith");
+      s1.setID(4);
+   }
+}
